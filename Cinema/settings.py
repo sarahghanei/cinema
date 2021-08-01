136a2ccd73915826d3d6cba11c9c459615f75fbf
Sarah Ghanei Ardakani <sarahghanei1997@gmail.com>
2021-08-01 12:30:51 +0430

    Update settings.py

--- Cinema/settings.py
+++ Cinema/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-k4w!jsk(hl4icukxvoakvuepyn0mli49s3=k!73v&q&#v)^d#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
